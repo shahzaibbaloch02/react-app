f836e4727eb45d11561303393b83bf18e4ec3469
shahzaibbaloch02 <shahzaib.baloch02@outlook.com>
2023-08-26 02:01:15 +0500

    2nd Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is Just a Test Project.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning React
         </a>
       </header>
     </div>
